e427924d9f0de0dea8d3938f9241f2b4a4354d6e
Sabelli <sabellinho@gmail.com>
2026-02-12 22:11:33 -0300

    Ex 3.2 - Acessando propriedades não existentes (undefined)

--- ex3_2.js
+++ ex3_2.js
@@ -1,7 +1,4 @@
-let produto = { nome: "Teclado", preco: 150 };
-console.log(produto.nome);        // Teclado
-console.log(produto.preco);       // 150
-console.log(produto.desconto);    // undefined — propriedade não existe
-// Adicionando a propriedade depois:
-produto.desconto = 20;
-console.log(produto.desconto);    // 20
+let aluno = { nome: "Victor", matricula: 2026 };
+console.log(aluno.nota);
+aluno.nota = 10;
+console.log(aluno.nota);
