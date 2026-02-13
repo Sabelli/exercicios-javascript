3f7cf60bdcfcf9017068ca6055f00a043940d357
Sabelli <sabellinho@gmail.com>
2026-02-12 22:59:03 -0300

    Ex 7.1 - Utilizando funções com escopo, callback e async para testes com console.log

--- ex7_1.js
+++ ex7_1.js
@@ -1,21 +1,23 @@
-let global = "Sou global";
-function testarEscopo() {
-  let local = "Sou local";
-  console.log(global);           // Funciona
-  console.log(local);            // Funciona
+// Parte A - Escopo
+let app = "MinhaApp";
+function funcaoTeste() {
+  let versao = "1.0";
+  console.log(app);
+  console.log(versao);
 }
-testarEscopo();
-console.log(global);             // Funciona
-// console.log(local);           // ❌ Erro — fora do escopo
+funcaoTeste();
+console.log(app);
+// console.log(versao); // não funciona porque a variável versao foi criada dentro da função testarEscopo, e não na "raíz" do código
 
-function processar(callback) {
-  console.log("Processando...");
+// Parte B - Callback
+function executarTarefa(nomeTarefa, callback) {
+  console.log("Iniciando: " + nomeTarefa);
   callback();
 }
-processar(() => console.log("Finalizado!"));
-// Saída: Processando... / Finalizado!
+executarTarefa("Lavar Roupa", () => console.log("Tarefa concluída!"));
 
-async function pegarDados() {
-  return "Dados recebidos!";
+// Parte C - Async
+async function buscarPerfil() {
+  return "Perfil carregado: Usuário Padrão";
 }
-pegarDados().then(console.log);  // Dados recebidos!
+buscarPerfil().then(console.log);
