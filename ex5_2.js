47a64aafbe7d850c9df50e363b17cf16c21b0bc3
Sabelli <sabellinho@gmail.com>
2026-02-12 22:25:13 -0300

    Ex 5.2 - Utilizando switch para exibir dia da semana

--- ex5_2.js
+++ ex5_2.js
@@ -1,15 +1,26 @@
-let cor = "azul";
-switch (cor) {
-  case "vermelho":
-    console.log("Parar!");
+let diaSemana = 2;
+switch (diaSemana) {
+  case 1:
+    console.log("Segunda-feira");
     break;
-  case "amarelo":
-    console.log("Atenção!");
+  case 2:
+    console.log("Terça-feira");
     break;
-  case "azul":
-    console.log("Cor favorita selecionada!");
+  case 3:
+    console.log("Quarta-feira");
+    break;
+  case 4:
+    console.log("Quinta-feira");
+    break;
+  case 5:
+    console.log("Sexta-feira");
+    break;
+  case 6:
+    console.log("Sábado");
+    break;
+  case 7:
+    console.log("Domingo");
     break;
   default:
-    console.log("Cor não reconhecida.");
+    console.log("Dia inválido.");
 }
-// Saída: Cor favorita selecionada!
