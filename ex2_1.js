4e6f1e1f13aa6e153848f78b7a3ad366bdbb1524
Sabelli <sabellinho@gmail.com>
2026-02-12 21:51:32 -0300

    Exemplos de variáveis

---
+++ ex2_1.js
@@ -0,0 +1,7 @@
+const nomeApp = "MeuApp";       // Constante — não muda
+let versao = 1;                 // Pode mudar
+console.log(nomeApp);           // MeuApp
+console.log(versao);            // 1
+versao = 2;                     // Atualizando a versão
+console.log(versao);            // 2
+// nomeApp = "OutroApp";        // ❌ Isso causaria erro!
