b9072144aa3638f3a3f880082d891d5a5c812842
Sabelli <sabellinho@gmail.com>
2026-02-12 22:07:40 -0300

    Ex 3.1 - Utilizando variáveis de tipo primitivo

---
+++ ex3_1.js
@@ -0,0 +1,6 @@
+let enderecoEntrega = null;
+let numeroPedido;
+console.log("Pedido: "+ numeroPedido + " - Endereço:" +enderecoEntrega);
+enderecoEntrega = "Avenida Paulista Nº444 Apto 32";
+numeroPedido = 67;
+console.log("Pedido: "+ numeroPedido + " - Endereço:" +enderecoEntrega);
