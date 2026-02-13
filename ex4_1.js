aeea50586968b6e68377947caf790187d5b53b1b
Sabelli <sabellinho@gmail.com>
2026-02-12 22:12:00 -0300

    Exemplo arrays e objetos

---
+++ ex4_1.js
@@ -0,0 +1,7 @@
+let frutas = ["Maçã", "Banana", "Laranja"];
+console.log(frutas[0]);          // "Maçã"   — primeiro item
+console.log(frutas[2]);          // "Laranja" — terceiro item
+console.log(frutas.length);      // 3
+frutas.push("Uva");              // Adiciona ao final
+console.log(frutas.length);      // 4
+console.log(frutas[3]);          // "Uva"
