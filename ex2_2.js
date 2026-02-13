0a3cce13ac1a599745cb9e3a1bacf893690c17d1
Sabelli <sabellinho@gmail.com>
2026-02-12 22:00:02 -0300

    Ex 2.2 - Utilizando typeof junto de console.log para exibição dos tipos de variáveis

---
+++ ex2_2.js
@@ -0,0 +1,8 @@
+let nome = "Victor";
+let idade = 24;
+let javascript = true;
+let vazio;
+console.log(nome + " - Tipo: "+typeof(nome));
+console.log(idade + " - Tipo: "+typeof(idade));
+console.log(javascript + " - Tipo: "+typeof(javascript));
+console.log(vazio + " - Tipo: "+typeof(vazio));
