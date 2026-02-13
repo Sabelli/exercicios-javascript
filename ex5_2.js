c0fa27fba3f69fcc5969ce83cc5868f3705b4ee4
Sabelli <sabellinho@gmail.com>
2026-02-12 22:22:56 -0300

    Exemplo switch

---
+++ ex5_2.js
@@ -0,0 +1,15 @@
+let cor = "azul";
+switch (cor) {
+  case "vermelho":
+    console.log("Parar!");
+    break;
+  case "amarelo":
+    console.log("Atenção!");
+    break;
+  case "azul":
+    console.log("Cor favorita selecionada!");
+    break;
+  default:
+    console.log("Cor não reconhecida.");
+}
+// Saída: Cor favorita selecionada!
